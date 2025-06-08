8517031e1efcbc76642b455df6076f04ad11d85d
yrahhaou <yassinerahhaoui12@gmail.com>
2025-06-08 05:09:15 +0100

    update style

---
+++ frontend/scripts/pages/attend.js
@@ -0,0 +1,9 @@
+import { ourFrame } from "../../../framework/dom.js"
+
+const AttendPage = () => {
+    return ourFrame.createElement('main',{class:'main'},
+        ourFrame.createElement()
+    )
+}
+
+export default AttendPage
