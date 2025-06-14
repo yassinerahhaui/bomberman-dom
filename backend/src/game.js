b7b1fbffafa7ee6152aaca5fc99a52eb47ea1d19
yrahhaou <yassinerahhaoui12@gmail.com>
2025-06-14 14:36:22 +0100

    create Vec and Level classes oop

--- backend/src/game.js
+++ backend/src/game.js
@@ -1,15 +1,39 @@
-class level { /* convert string to slice */
-    constructor(parameters) {
-
-    }
+const levelChars = {
+  ".": "empty",
+  "#": "wall",
+};
+class Level {
+  /* convert string to slice */
+  constructor(map) {
+    let rows = map.trim.split("\n").map((line) => [...line]);
+    this.height = rows.length;
+    this.width = rows[0].length;
+    this.startActors = [];
+    this.rows = rows.map((row, y) => {
+      return row.map((ch, x) => {
+        let type = levelChars[ch];
+        if (typeof type === "string") return type;
+        this.startActors.push(type.create(new Vec(x, y), ch));
+      });
+    });
+  }
 }
 
 class Player {
-    constructor(position, speed) {
-
-    }
+  constructor(position, speed) {}
 }
 
-class Vector {
+class Vec {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
 
+  plus(other) {
+    return new Vec(this.x + other.x, this.y + other.y);
+  }
+
+  times(factor) {
+    return new Vec(this.x * factor, this.y * factor);
+  }
 }
